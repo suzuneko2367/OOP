da623ff64a51a3030a93d597023be689a883cc5a
鈴猫 <suzuneko2367@gmail.com>
2026-04-13 23:42:33 +0900

    Create bai 1.2.cpp

---
+++ bai 1.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+  float m=42;
+  float s=42;
+  float tong1=m*60+s;
+  cout<<tong1<<endl;
+  float k=10;
+  float mile=k/1.61;
+  cout<<mile<<endl;
+  float tong2=tong1/60;
+  float ap1= tong2/mile;
+  int phut=(int)tong;
+  int giay=(int)(ap1-phut)*60;
+  cout<<phut<<" m "<<giay<<" s "<<endl;
+  float tong3=tong2/60;
+  float v=mile/tong3;
+  cout<<v;
+}
